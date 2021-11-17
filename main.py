cf61ea2dd78c72b82d48cd4d5fceac408d2ab50d
Gilberto Charles <gilbertocharles1@gmail.com>
2021-11-17 19:08:12 -0300

    comit inicial

---
+++ main.py
@@ -0,0 +1,16 @@
+
+class AG():
+    def __init__(self) -> None:
+        pass
+
+    def ola_mundo() -> None:
+        print('Olá, mundo!')
+
+def main():
+    ag = AG
+    ag.ola_mundo()
+
+
+if __name__ == '__main__':
+    main()
+    
